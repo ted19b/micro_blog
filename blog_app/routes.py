05dbd2d5436c1972751e476d9449cf45a4e5a829
ted19b <ted@hitech4every1.de>
2019-08-10 00:43:37 +0200

    add a login template for users

--- blog_app/routes.py
+++ blog_app/routes.py
@@ -1,5 +1,8 @@
 from blog_app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+
+from blog_app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +19,16 @@ def index():
         }
     ]
     return render_template('home.html', posts=posts, user=mock_user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title='Sign In', form=form)
